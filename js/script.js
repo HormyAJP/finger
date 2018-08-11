7f1ae67904c6c1afd05c201a0cfd8a66a698f012
Andrew Parker <andrew.j.c.parker@gmail.com>
2018-08-11 19:43:39 +0800

    Use all math

--- js/script.js
+++ js/script.js
@@ -2,5 +2,22 @@ function log(text) {
     document.getElementById("log").innerHTML += text + "<br>"
 }
 
-log(Math.cos(1))
+sum = Math.acos(0.5) +
+    Math.acosh(1) +
+    Math.asin(0.5) +
+    Math.asinh(1) +
+    Math.atan(1) +
+    Math.atanh(0.5) +
+    Math.cbrt(2) +
+    Math.cos(1) +
+    Math.cosh(1) +
+    Math.exp(1.5) +
+    Math.log(10) +
+    Math.log10(Math.E) +
+    Math.pow(Math.E, Math.PI) +
+    Math.sin(1) +
+    Math.sinh(1) +
+    Math.tan(1) +
+    Math.tanh(1);
 
+log(sum)
